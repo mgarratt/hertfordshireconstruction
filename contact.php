4a2ba42aec9fbcd81ba36765a92d38b5a2276d38
Mark Garratt <mgarratt@gmail.com>
2013-06-23 11:18:43 +0100

    Fix unclosed H1 tags

--- contact.php
+++ contact.php
@@ -61,7 +61,7 @@
                 <p class="contact"><span>Phone:</span> 01462 434 761<br />
                     <span>Mobile:</span>  07725 081 240<br />
                     <span>Email:</span> <a href="mailto:info@hertfordshireconstruction.co.uk">info@hertfordshireconstruction.co.uk</a></p>
-                <p>Hertfordshire Constructions services are available in Hitchin and surrounding areas<p>
+                <p>Hertfordshire Construction's services are available in Hitchin and surrounding areas<p>
                 <img src="http://maps.google.com/maps/api/staticmap?size=690x410&path=fillcolor:0x00FF00|weight:1|color:0xFFFFFF|enc:g|g~Hjjv@nDs~FnOk}FjZ}zFbe@ewFvo@grF~y@elFbdA{dFzmAo|EbwAasE``BqhEnhBa}DjpBupDxwBmcDp~BkuCtdCofCdjCcwB~nC_gBdsCovApvCoeAdyCet@b{Csb@f|CyPt|C?f|CxPb{Crb@dyCdt@pvCneAdsCnvA~nC~fBdjCbwBtdCnfCp~BjuCxwBlcDjpBtpDnhB`}D``BphEbwA`sEzmAn|EbdAzdF~y@dlFvo@frFbe@dwFjZ|zFnOj}FnDr~FoDr~FoOj}FkZ|zFce@dwFwo@frF_z@blFcdA|dF{mAn|EcwA`sEa`BphEohB`}DkpBtpDywBlcDq~BhuCudCpfCejC`wB_oC`gBesCnvAqvCneAeyCdt@c{Cpb@g|CzPu|C?g|C{Pc{Cqb@eyCet@qvCoeAesCovA_oCagBejCawBudCqfCq~BiuCywBmcDkpBupDohBa}Da`BqhEcwAasE{mAo|EcdA}dF_z@clFwo@grFce@ewFkZ}zFoOk}FoDs~F&sensor=true" class="aligncenter" />
             </div>
             <div class="col4 box">
